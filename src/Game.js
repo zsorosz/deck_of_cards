fb7309c6770e3776fcdb27fbd089a08929f697c0
Zsofia <oroszi.zsofi@gmail.com>
2020-04-06 17:09:54 +0200

    fetch new card from API

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import axios from "axios";
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Game extends Component {
     constructor(props){
         super(props);
         this.state = { deck: null };
+        this.getCard = this.getCard.bind(this);
     }
     async componentDidMount(){
-        let deck = await axios.get(API_URL);
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
         this.setState({ deck: deck.data });
+    }
+    async getCard(){
+        let id = this.state.deck.deck_id;
+        let cardUrl = `${API_BASE_URL}/${id}/draw`;
+        let cardRes = await axios.get(cardUrl);
+        console.log(cardRes);
+        let card = cardRes.data.card[0];
     }
     render(){
         return(
-            <h1>Card Dealer</h1>
+            <div>
+                <h1>Card Dealer</h1>
+                <button onClick={this.getCard}>Get Card!</button>
+            </div>
         )
     }
 }
