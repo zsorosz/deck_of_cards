0e9ec3b608226de3e0876c818ef0fcc204c94306
Zsofia <oroszi.zsofi@gmail.com>
2020-04-06 14:42:46 +0200

    install axios, make API request

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import axios from "axios";
+const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/';
 
 class Game extends Component {
+    constructor(props){
+        super(props);
+        this.state = { deck: null };
+    }
+    async componentDidMount(){
+        let deck = await axios.get(API_URL);
+        this.setState({ deck: deck.data });
+    }
     render(){
         return(
-            <h1>Hello from the Game</h1>
+            <h1>Card Dealer</h1>
         )
     }
 }
