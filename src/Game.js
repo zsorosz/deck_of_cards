1c079fd34a708e6239455a4073e2af55160d2dbf
Zsofia <oroszi.zsofi@gmail.com>
2020-04-06 13:15:16 +0200

    set up first component

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render(){
+        return(
+            <h1>Hello from the Game</h1>
+        )
+    }
+}
+
+export default Game;
