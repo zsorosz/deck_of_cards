8e95be7b8b90ffa566f2dd867b126ed2d6b5362f
Zsofia <oroszi.zsofi@gmail.com>
2020-04-07 17:19:13 +0200

    add card component

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from "./Card";
 import axios from "axios";
 const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
@@ -37,10 +38,14 @@ class Game extends Component {
         }    
     }
     render(){
+        const cards = this.state.drawn.map(c => (
+            <Card key={c.id} name={c.name} image={c.image} />
+        ));
         return(
             <div>
                 <h1>Card Dealer</h1>
                 <button onClick={this.getCard}>Get Card!</button>
+                {cards}
             </div>
         )
     }
